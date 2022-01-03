26c72768de891d60a6ba90ff877623d62fbbe1b6
Konstantin Cherenuk <cherenuk05@gmail.com>
2022-01-03 20:10:55 +0700

    make showing of a result on a canvas

--- gui-water_heating.py
+++ gui-water_heating.py
@@ -20,8 +20,8 @@ FONT_ENTRY = 'Arial 14'
 FONT_COMMANDS = 'Arial 12 italic'
 
 title = 'Калькулятор расчета времени кипения воды \n на газовой горелке'
-ready_text = '— Нажмите готово для получения результата'
-graph_text = '— График, чтобы увидеть зависимость температуры \n от времени'
+ready_text = 'ᐅ  Нажмите готово для получения результата'
+graph_text = 'ᐅ  График, чтобы увидеть зависимость температуры \n от времени'
 
 
 def enter_color(event, but):
@@ -37,7 +37,7 @@ def result(boiler_index, boiler_mass, start_temp, burner_power, water_volume):
     return time
 
 
-def show_dialog(time, start_temp):
+def show_result(canvas, item1, item2, item3, time, start_temp):
     text = f'''Время кипения воды {"%.2f" % time} мин.
 Нарисовать график зависимости температуры от времени?'''
 
@@ -47,6 +47,13 @@ def show_dialog(time, start_temp):
                 )
     if answer:
         graph_temp.make_graph(start_temp, 100, time)
+
+    canvas.delete(item1, item2, item3)
+    canvas.create_text(220, 40, text=f'Время кипения воды: \n \n {"%.2f" % time} мин', justify=tk.CENTER, font=FONT_ENTRY)
+
+
+def show_graph(start_temp, time):
+    graph_temp.make_graph(start_temp, 100, time)
 
 
 def main():
@@ -66,9 +73,9 @@ def main():
 
     figure = tk.Canvas(window, bg='white', bd=3, highlightthickness=1, relief='ridge')
     figure.place(x=0, y=0, relx=0, rely=0, width=W_FIGURE, height=H-20)
-    figure.create_text(220, 40, text=title, justify=tk.CENTER, font=FONT_ENTRY)
-    figure.create_text(200, 110, text=ready_text, font=FONT_COMMANDS)
-    figure.create_text(230, 170, text=graph_text, justify=tk.CENTER, font=FONT_COMMANDS)
+    title_look = figure.create_text(220, 40, text=title, justify=tk.CENTER, font=FONT_ENTRY)
+    ready_look = figure.create_text(200, 110, text=ready_text, font=FONT_COMMANDS)
+    graph_look = figure.create_text(230, 170, text=graph_text, justify=tk.CENTER, font=FONT_COMMANDS)
     figure.create_image(W_FIGURE//2, H-150, image=bowler_img)
 
     material = tk.Label(font=FONT, text='Материал котелка/чайника:')
@@ -102,7 +109,10 @@ def main():
                       relief='raised', borderwidth=3)
     ready.bind('<Enter>', lambda e: enter_color(e, ready))
     ready.bind('<Leave>', lambda e: leave_color(e, ready))
-    ready['command'] = lambda : show_dialog(result(material_list.get(), int(mass.get()), int(temp.get()), int(power.get()), int(volume.get())), int(temp.get()))
+    ready['command'] = lambda : show_result(figure, title_look, ready_look, graph_look,
+                                            result(material_list.get(), int(mass.get()),
+                                                   int(temp.get()), int(power.get()),
+                                                   int(volume.get())), int(temp.get()))
 
     ready.grid(row=10, column=0, pady=20, padx=10, sticky=tk.E)
 
@@ -111,6 +121,9 @@ def main():
     graph_but.bind('<Enter>', lambda e: enter_color(e, graph_but))
     graph_but.bind('<Leave>', lambda e: leave_color(e, graph_but))
     graph_but.grid(row=11, column=0, padx=10, sticky=tk.E)
+    graph_but['command'] = lambda : show_graph(int(temp.get()), result(material_list.get(), int(mass.get()),
+                                                   int(temp.get()), int(power.get()),
+                                                   int(volume.get())))
 
     window.columnconfigure(0, weight=1)
 
