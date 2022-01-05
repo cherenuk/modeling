e242878da836a3347140e09ebfc11267f85b0e0b
Konstantin Cherenuk <cherenuk05@gmail.com>
2022-01-05 17:43:06 +0700

    make right showing of a result on a canvas

--- gui-water_heating.py
+++ gui-water_heating.py
@@ -16,10 +16,13 @@ W_FIGURE = 460
 FONT = 'Arial 12'
 FONT_ENTRY = 'Arial 14'
 FONT_COMMANDS = 'Arial 12 italic'
+FONT_RESULT = 'Arial 14 italic'
 
 title = 'Калькулятор расчета времени кипения воды \n на газовой горелке'
 ready_text = 'ᐅ  Нажмите готово для получения результата'
 graph_text = 'ᐅ  График, чтобы увидеть зависимость температуры \n от времени'
+extra_text = 'ᐅ  В верхнем меню есть дополнительная информация'
+test_text='---------------------------------------------------------------------------'
 
 
 def enter_color(event, but):
@@ -37,8 +40,8 @@ def result(boiler_index, boiler_mass, start_temp, burner_power, water_volume):
 
 def show_result(canvas, item1, item2, item3, time, start_temp):
     canvas.delete(item1, item2, item3)
-    canvas.create_text(220, 40, text=f'Время кипения воды: \n \n {"%.2f" % time} мин',
-                       justify=tk.CENTER, font=FONT_ENTRY)
+    canvas.create_text(220, 110, text=f'Время кипения воды: \n \n ➤{"%.2f" % time} мин',
+                       justify=tk.CENTER, font=FONT_RESULT)
 
 
 def show_graph(start_temp, time):
@@ -62,9 +65,11 @@ def main():
 
     figure = tk.Canvas(window, bg='white', bd=3, highlightthickness=1, relief='ridge')
     figure.place(x=0, y=0, relx=0, rely=0, width=W_FIGURE, height=H-20)
-    title_look = figure.create_text(220, 40, text=title, justify=tk.CENTER, font=FONT_ENTRY)
-    ready_look = figure.create_text(200, 110, text=ready_text, font=FONT_COMMANDS)
-    graph_look = figure.create_text(230, 170, text=graph_text, justify=tk.CENTER, font=FONT_COMMANDS)
+    title_look = figure.create_text(220, 30, text=title, justify=tk.CENTER, font=FONT_ENTRY)
+    test_look = figure.create_text(230, 55, text=test_text, justify=tk.CENTER, font=FONT_ENTRY)
+    ready_look = figure.create_text(200, 80, text=ready_text, font=FONT_COMMANDS)
+    graph_look = figure.create_text(230, 140, text=graph_text, justify=tk.CENTER, font=FONT_COMMANDS)
+    extra_look = figure.create_text(225, 200, text=extra_text, justify=tk.CENTER, font=FONT_COMMANDS)
     figure.create_image(W_FIGURE//2, H-150, image=bowler_img)
 
     material = tk.Label(font=FONT, text='Материал котелка/чайника:')
@@ -98,23 +103,24 @@ def main():
                       relief='raised', borderwidth=3)
     ready.bind('<Enter>', lambda e: enter_color(e, ready))
     ready.bind('<Leave>', lambda e: leave_color(e, ready))
-    ready['command'] = lambda : show_result(figure, title_look, ready_look, graph_look,
+    ready['command'] = lambda : show_result(figure, ready_look, graph_look, extra_look,
                                             result(material_list.get(), int(mass.get()),
                                                    int(temp.get()), int(power.get()),
                                                    int(volume.get())), int(temp.get()))
 
-    ready.grid(row=10, column=0, pady=20, padx=10, sticky=tk.E)
+    ready.grid(row=10, column=0, padx=10, sticky=tk.SE)
 
     graph_but = tk.Button(text='График', bg=BLUE, width=30, height=2,
                       relief='raised', borderwidth=3)
     graph_but.bind('<Enter>', lambda e: enter_color(e, graph_but))
     graph_but.bind('<Leave>', lambda e: leave_color(e, graph_but))
-    graph_but.grid(row=11, column=0, padx=10, sticky=tk.E)
-    graph_but['command'] = lambda : show_graph(int(temp.get()), result(material_list.get(), int(mass.get()),
-                                                   int(temp.get()), int(power.get()),
-                                                   int(volume.get())))
+    graph_but['command'] = lambda: show_graph(int(temp.get()), result(material_list.get(), int(mass.get()),
+                                                                      int(temp.get()), int(power.get()),
+                                                                      int(volume.get())))
+    graph_but.grid(row=11, column=0, padx=10, pady=10, sticky=tk.SE)
 
     window.columnconfigure(0, weight=1)
+    window.rowconfigure(10, weight=1)
 
     window.mainloop()
 
